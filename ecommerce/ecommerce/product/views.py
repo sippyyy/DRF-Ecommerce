ffe51e8d115210d308a45f05d17684b4f513dc00
sippyyy <nguyenthuy030800@gmail.com>
2023-09-10 23:03:34 +0700

    add table Product, Brand, Category

---
+++ ecommerce/ecommerce/product/views.py
@@ -0,0 +1,12 @@
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Category
+from .serializers import CategorySerializer
+
+
+class CategoryView(viewsets.ViewSet):
+    queryset = Category.objects.all()
+
+    def list(self, request):
+        serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
